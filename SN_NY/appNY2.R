b6d48871524d1294dbbbbc4f735c2a928d5d5fa5
lbouzoubaa <lab218@miami.edu>
2019-05-17 07:57:15 -0400

    added remainder of neighborhood functions

--- SN_NY/appNY2.R
+++ SN_NY/appNY2.R
@@ -82,6 +82,25 @@ ui <- fluidPage(
                ),
                p("Waffle plots are an alternative to pie charts.  They are made of a 10 by 10 gird. Every block in the waffle represents 1%.",
                  style = "font-size: 12px; font-style: italic")
+    ),
+    br(),
+    wellPanel( h3("Neighborhood Snapshot", style = "font-style: bold" ),
+               br(),
+               h5("Population: ", style = "display:inline; font-size: 18px; font-style: bold; color: #000000"),
+               textOutput("totalpop", inline = TRUE), tags$style("#totalpop{font-size: 18px; font-style: bold; color: #F26B68}"),
+               br(),
+               br(),
+               h5("Population Under 18: ", style = "display:inline; font-size: 18px; font-style: bold; color: #000000"),
+               textOutput("under18", inline = TRUE), tags$style("#under18{font-size: 18px;  font-style: bold; color: #F26B68}"),
+               br(),
+               h5("Population Over 65: ", style = "display:inline; font-size: 18px; font-style: bold; color: #000000"),
+               textOutput("over65", inline = TRUE), tags$style("#over65{font-size: 18px;  font-style: bold; color: #F26B68}"),
+               br(),
+               h5("Median Income: ", style = "display:inline; font-size: 18px; font-style: bold; color: #000000"),
+               textOutput("income", inline = TRUE), tags$style("#income{font-size: 18px; font-style: bold; color: #F26B68}"),
+               br(),
+               h5("Unemployment Rate: ", style = "display:inline; font-size: 18px; font-style: bold; color: #000000"),
+               textOutput("unemployment", inline = TRUE), tags$style("#unemployment{font-size: 18px; font-style: bold; color: #F26B68}")
     )
   ), # end sidebarPanel
   
@@ -102,73 +121,73 @@ ui <- fluidPage(
                ),
                br(),
                fluidRow(
-                 # column(6, plotOutput("englishPlot", height = 250)),
-                 # column(6, plotOutput("nativePlot", height = 250))
+                 column(6, plotOutput("englishPlot", height = 250)),
+                 column(6, plotOutput("nativePlot", height = 250))
                ),
                br(),
                fluidRow(
-                 # column(6, plotOutput("educationPlot", height = 250))
+                  column(6, plotOutput("educationPlot", height = 250))
                ),
                br(),
                fluidRow(
-                 # column(8,  offset = 2, plotOutput("agePlot", height = 600))
+                 column(8, offset = 2, plotOutput("agePlot", height = 600))
                )
                
       ), # end tabPanel Demographics
       tabPanel("Housing", style = "background-color: #F2EEEC;",
                fluidRow(
                  # column(6, plotOutput("ownRentPlot", height = 250)),
-                 # column(6, plotOutput("vacancyPlot", height = 250))
+                  column(6, plotOutput("vacancyPlot", height = 250))
                ),
                br(),
                fluidRow(
-                 # column(6, plotOutput("yearBuiltPlot", height = 250)),
-                 # column(6, plotOutput("urbanRuralPlot", height = 250))
+                 column(6, plotOutput("yearBuiltPlot", height = 250)),
+                 column(6, plotOutput("urbanRuralPlot", height = 250))
                )
       ), # end tabPanel Housing
       tabPanel("Economic", style = "background-color: #F2EEEC;",
                fluidRow(
-                 column(6, plotOutput("commutePlot", height = 250))
-                 #column(6, plotOutput("povertyPlot", height = 250))
+                 column(6, plotOutput("commutePlot", height = 250)),
+                 column(6, plotOutput("povertyPlot", height = 250))
                  
                ),
                br(),
                fluidRow(
-                 #column(6, plotOutput("rentincomePlot", height = 250)),
+                 column(6, plotOutput("rentincomePlot", height = 250)),
                  column(6, plotOutput("vehiclePlot", height = 250))
                ),
-               br()
-               # fluidRow(
-               #   column(6, plotOutput("unemploymentPlot", height = 250))
-               #
-               # )
+               br(),
+               fluidRow(
+                 column(6, plotOutput("unemploymentPlot", height = 250))
+
+               )
       ), # end tabPanel Economic
       
       tabPanel("Insurance", style = "background-color: #F2EEEC;",
                fluidRow(
                  
-                 # column(2,
-                 # radioButtons(
-                 #   inputId = "insurance",
-                 #   "Show insurance by:",
-                 #   choices = list("Age and Sex" = 2,
-                 #                  "Age" = 4,
-                 #                  "Sex" = 6))),
-                 #
-                 # column(6, plotOutput("insurancePlot", height = 500))
+                 column(2,
+                 radioButtons(
+                   inputId = "insurance",
+                   "Show insurance by:",
+                   choices = list("Age and Sex" = 2,
+                                  "Age" = 4,
+                                  "Sex" = 6))),
+
+                 column(6, plotOutput("insurancePlot", height = 500))
                ),
                br(),
                fluidRow(
                  
-                 # column(2,
-                 #        radioButtons(
-                 #          inputId = "insuranceType",
-                 #          "Show insurance type by:",
-                 #          choices = list("Age and Sex" = 2,
-                 #                         "Age" = 4,
-                 #                         "Sex" = 6))),
-                 #
-                 # column(10, plotOutput("insuranceTypePlot", height = 500))
+                 column(2,
+                        radioButtons(
+                          inputId = "insuranceType",
+                          "Show insurance type by:",
+                          choices = list("Age and Sex" = 2,
+                                         "Age" = 4,
+                                         "Sex" = 6))),
+
+                 column(10, plotOutput("insuranceTypePlot", height = 500))
                )
       )#end tab Insurance
     ) #end tabset panel
@@ -201,7 +220,6 @@ server <- function(input, output, session) {
     }
   })
   
-  # added for showing districts
   output$theCounty <- reactive(
     theCounty()
   )
@@ -216,10 +234,99 @@ server <- function(input, output, session) {
   })
   
   
-  # added for showing districts
   output$thePlace <- reactive(
     thePlace()
   )
+  
+  #####NEIGHBORHOOD SNAPSHOT OUTPUTS  
+  output$totalpop <-
+    renderText({
+      req(theCounty())
+      req(thePlace())
+      req(input$mode)
+      
+      
+      if(theCounty() == 36) {
+        plot_over65(geo = "state", geoId = 36)$data[4]
+      }else if (theCounty() != 36 & thePlace() == -1){
+        plot_over65(geo = "county", geoId = theCounty())$data[4]
+      }else(
+        plot_over65(geo = "place", geoId = thePlace())$data[4]
+      )
+    })
+  
+  
+  output$under18 <-
+    renderText({
+      req(theCounty())
+      req(thePlace())
+      req(input$mode)
+      
+      
+      if(theCounty() == 36) {
+        paste0(plot_over65(geo = "state", geoId = 36)$data[2], "%")
+      }else if (theCounty() != 36 & thePlace() == -1){
+        paste0(plot_over65(geo = "county", geoId = theCounty())$data[2], "%")
+      }else(
+        paste0(plot_over65(geo = "place", geoId = thePlace())$data[2], "%")
+      )
+    })
+  
+  output$over65 <-
+    renderText({
+      req(theCounty())
+      req(thePlace())
+      req(input$mode)
+      
+      
+      if(theCounty() == 36) {
+        paste0(plot_over65(geo = "state", geoId = 36)$data[1], "%")
+      }else if (theCounty() != 36 & thePlace() == -1){
+        paste0(plot_over65(geo = "county", geoId = theCounty())$data[1], "%")
+      }else(
+        paste0(plot_over65(geo = "place", geoId = thePlace())$data[1], "%")
+      )
+    })
+  
+  output$income <-
+    renderText({
+      req(theCounty())
+      req(thePlace())
+      req(input$mode)
+      
+      
+      if(theCounty() == 36) {
+        plot_income(geo = "state", geoId = 36)$data[1]
+      }else if (theCounty() != 36 & thePlace() == -1){
+        plot_income(geo = "county", geoId = theCounty())$data[1]
+      }else(
+        plot_income(geo = "place", geoId = thePlace())$data[1]
+      )
+    })
+  
+  output$unemployment <-
+    renderText({
+      req(theCounty())
+      req(thePlace())
+      req(input$mode)
+      
+      
+      if(theCounty() == 36) {
+        paste0(round((1 - plot_unemployment(geo = "state", 
+                                            geoId = 36)$data[1]) * 100 ),
+               "%")
+      }else if (theCounty() != 36 & thePlace() == -1){
+        paste0(round((1 - plot_unemployment(geo = "county", 
+                                            geoId = theCounty())$data[1]) * 100 ),
+               "%")
+      }else(
+        paste0(round((1 - plot_unemployment(geo = "place", 
+                                            geoId = thePlace())$data[1]) * 100 ),
+               "%")
+      )
+    })
+  
+  #####LEAFLET MAP
   
   output$working_map <- renderLeaflet({
     req(theCounty())
@@ -243,6 +350,8 @@ server <- function(input, output, session) {
       )
       
   }) # End working_map renderLeaflet
+  
+  #####CENSUS VARIABLES WAFFLE/BAR OUTPUTS
   
   output$ethnicityPlot <- renderPlot({
     req(theCounty())
@@ -278,6 +387,125 @@ server <- function(input, output, session) {
     
   }, bg = "transparent")
   
+  output$englishPlot <- renderPlot({
+    req(theCounty())
+    req(thePlace())
+    req(input$mode)
+    
+    
+    if(theCounty() == 36) {
+      grid.draw(plot_english(geo = "state", geoId = 36)[[ as.numeric(input$mode) ]])
+    }else if (theCounty() != 36 & thePlace() == -1){
+      grid.draw(plot_english(geo = "county", geoId = theCounty())[[ as.numeric(input$mode) ]])
+    }else(
+      grid.draw(plot_english(geo = "place", geoId = thePlace())[[ as.numeric(input$mode) ]])
+    )
+    
+    
+  }, bg = "transparent")
+  
+  output$nativePlot <- renderPlot({
+    req(theCounty())
+    req(thePlace())
+    req(input$mode)
+    
+    
+    if(theCounty() == 36) {
+      grid.draw(plot_native(geo = "state", geoId = 36)[[ as.numeric(input$mode) ]])
+    }else if (theCounty() != 36 & thePlace() == -1){
+      grid.draw(plot_native(geo = "county", geoId = theCounty())[[ as.numeric(input$mode) ]])
+    }else(
+      grid.draw(plot_native(geo = "place", geoId = thePlace())[[ as.numeric(input$mode) ]])
+    )
+    
+    
+  }, bg = "transparent")
+  
+  output$educationPlot <- renderPlot({
+    req(theCounty())
+    req(thePlace())
+    req(input$mode)
+    
+    
+    if(theCounty() == 36) {
+      grid.draw(plot_education(geo = "state", geoId = 36)[[ as.numeric(input$mode) ]])
+    }else if (theCounty() != 36 & thePlace() == -1){
+      grid.draw(plot_education(geo = "county", geoId = theCounty())[[ as.numeric(input$mode) ]])
+    }else(
+      grid.draw(plot_education(geo = "place", geoId = thePlace())[[ as.numeric(input$mode) ]])
+    )
+    
+    
+  }, bg = "transparent")
+  
+  output$agePlot <- renderPlot({
+    req(theCounty())
+    req(thePlace())
+    req(input$mode)
+    
+    
+    if(theCounty() == 36) {
+      plot_age(geo = "state", geoId = 36)
+    }else if (theCounty() != 36 & thePlace() == -1){
+      plot_age(geo = "county", geoId = theCounty())
+    }else(
+      plot_age(geo = "place", geoId = thePlace())
+    )
+    
+    
+  }, bg = "transparent")
+  
+  output$vacancyPlot <- renderPlot({
+    req(theCounty())
+    req(thePlace())
+    req(input$mode)
+    
+    
+    if(theCounty() == 36) {
+      grid.draw(plot_vacancy(geo = "state", geoId = 36)[[ as.numeric(input$mode) ]])
+    }else if (theCounty() != 36 & thePlace() == -1){
+      grid.draw(plot_vacancy(geo = "county", geoId = theCounty())[[ as.numeric(input$mode) ]])
+    }else(
+      grid.draw(plot_vacancy(geo = "place", geoId = thePlace())[[ as.numeric(input$mode) ]])
+    )
+    
+    
+  }, bg = "transparent")
+  
+  output$yearBuiltPlot <- renderPlot({
+    req(theCounty())
+    req(thePlace())
+    req(input$mode)
+    
+    
+    if(theCounty() == 36) {
+      grid.draw(plot_year_built(geo = "state", geoId = 36)[[ as.numeric(input$mode) ]])
+    }else if (theCounty() != 36 & thePlace() == -1){
+      grid.draw(plot_year_built(geo = "county", geoId = theCounty())[[ as.numeric(input$mode) ]])
+    }else(
+      grid.draw(plot_year_built(geo = "place", geoId = thePlace())[[ as.numeric(input$mode) ]])
+    )
+    
+    
+  }, bg = "transparent")
+  
+  output$urbanRuralPlot <- renderPlot({
+    req(theCounty())
+    req(thePlace())
+    req(input$mode)
+    
+    
+    if(theCounty() == 36) {
+      grid.draw(plot_urbanRural(geo = "state", geoId = 36)[[ as.numeric(input$mode) ]])
+    }else if (theCounty() != 36 & thePlace() == -1){
+      grid.draw(plot_urbanRural(geo = "county", geoId = theCounty())[[ as.numeric(input$mode) ]])
+    }else(
+      grid.draw(plot_urbanRural(geo = "place", geoId = thePlace())[[ as.numeric(input$mode) ]])
+    )
+    
+    
+  }, bg = "transparent")
+  
   output$commutePlot <- renderPlot({
     req(theCounty())
     req(thePlace())
@@ -295,6 +523,40 @@ server <- function(input, output, session) {
     
   }, bg = "transparent")
   
+  output$povertyPlot <- renderPlot({
+    req(theCounty())
+    req(thePlace())
+    req(input$mode)
+    
+    
+    if(theCounty() == 36) {
+      grid.draw(plot_poverty(geo = "state", geoId = 36)[[ as.numeric(input$mode) ]])
+    }else if (theCounty() != 36 & thePlace() == -1){
+      grid.draw(plot_poverty(geo = "county", geoId = theCounty())[[ as.numeric(input$mode) ]])
+    }else(
+      grid.draw(plot_poverty(geo = "place", geoId = thePlace())[[ as.numeric(input$mode) ]])
+    )
+    
+    
+  }, bg = "transparent")
+  
+  output$rentincomePlot <- renderPlot({
+    req(theCounty())
+    req(thePlace())
+    req(input$mode)
+    
+    
+    if(theCounty() == 36) {
+      grid.draw(plot_rentincome(geo = "state", geoId = 36)[[ as.numeric(input$mode) ]])
+    }else if (theCounty() != 36 & thePlace() == -1){
+      grid.draw(plot_rentincome(geo = "county", geoId = theCounty())[[ as.numeric(input$mode) ]])
+    }else(
+      grid.draw(plot_rentincome(geo = "place", geoId = thePlace())[[ as.numeric(input$mode) ]])
+    )
+    
+    
+  }, bg = "transparent")
+  
   
   output$vehiclePlot <- renderPlot({
     req(theCounty())
@@ -307,6 +569,54 @@ server <- function(input, output, session) {
       grid.draw(plot_vehicle(geo = "county", geoId = theCounty())[[ as.numeric(input$mode) ]])
     }else(
       grid.draw(plot_vehicle(geo = "place", geoId = thePlace())[[ as.numeric(input$mode) ]])
+    )
+    
+    
+  }, bg = "transparent")
+  
+  output$unemploymentPlot <- renderPlot({
+    req(theCounty())
+    req(thePlace())
+    req(input$mode)
+    
+    if(theCounty() == 36) {
+      grid.draw(plot_unemployment(geo = "state", geoId = 36)[[ as.numeric(input$mode) ]])
+    }else if (theCounty() != 36 & thePlace() == -1){
+      grid.draw(plot_unemployment(geo = "county", geoId = theCounty())[[ as.numeric(input$mode) ]])
+    }else(
+      grid.draw(plot_unemployment(geo = "place", geoId = thePlace())[[ as.numeric(input$mode) ]])
+    )
+    
+    
+  }, bg = "transparent")
+  
+  output$insurancePlot <- renderPlot({
+    req(theCounty())
+    req(thePlace())
+    req(input$mode)
+    
+    if(theCounty() == 36) {
+      plot_insurance(geo = "state", geoId = 36)[[as.numeric(input$insurance)]]
+    }else if (theCounty() != 36 & thePlace() == -1){
+      plot_insurance(geo = "county", geoId = theCounty())[[as.numeric(input$insurance)]]
+    }else(
+      plot_insurance(geo = "place", geoId = thePlace())[[as.numeric(input$insurance)]]
+    )
+    
+    
+  }, bg = "transparent")
+  
+  output$insuranceTypePlot <- renderPlot({
+    req(theCounty())
+    req(thePlace())
+    req(input$mode)
+    
+    if(theCounty() == 36) {
+      plot_insurance_type(geo = "state", geoId = 36)[[as.numeric(input$insuranceType)]]
+    }else if (theCounty() != 36 & thePlace() == -1){
+      plot_insurance_type(geo = "county", geoId = theCounty())[[as.numeric(input$insuranceType)]]
+    }else(
+      plot_insurance_type(geo = "place", geoId = thePlace())[[as.numeric(input$insuranceType)]]
     )
     
     
